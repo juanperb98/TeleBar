71aaedf008b07ce983d3bb63338f29d1c3d9bf80
diego@alpha <diegorodriguezriera@gmail.com>
2019-10-14 19:48:52 +0200

    removed cmkae build

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/data_1/code/TeleBar
-# Build directory: /mnt/data_1/code/TeleBar/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
