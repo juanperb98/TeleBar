2765029369757215c53b3537ef14683c2344f1f5
diego@alpha <diegorodriguezriera@gmail.com>
2019-10-17 14:15:37 +0200

    added game to project and changed mainserver to server

---
+++ app/src/game.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <telebar/kernel/Client.hpp>
+#include <telebar/entity/User.hpp>
+
+int main(int argc, char const *argv[]) {
+    std::cout<<"hello, world form the game.\n";
+}
+
