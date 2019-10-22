0fa3e80890e2faf0ad5777d26cc175e03bd9bcec
diego@alpha <diegorodriguezriera@gmail.com>
2019-10-22 10:16:51 +0200

    renamed private variables

--- app/include/telebar/utils/ORM.hpp
+++ app/include/telebar/utils/ORM.hpp
@@ -11,8 +11,8 @@
 
 class ORM{
 private:
-    std::string stream;
-    sqlite3* db;
+    std::string stream_;
+    sqlite3* db_;
 
 private:
     /*static int genericCallback(void *NotUsed, int argc, char **argv, char **azColName) {
@@ -38,21 +38,21 @@ private:
 
 public:
     explicit ORM(const std::string& dbName) {
-        int rc = sqlite3_open(dbName.c_str(), &this->db);
+        int rc = sqlite3_open(dbName.c_str(), &this->db_);
 
         if( rc ) {
-            fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+            fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db_));
             exit(0);
         }
     };
 
     ~ORM() {
-        sqlite3_close(db);
+        sqlite3_close(db_);
     };
 
     template <class ObjectType>
     bool createTable() {
-        sqlite3_exec(this->db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
         ObjectType object;
         std::string querry = "CREATE TABLE "+ object.getTableName() +"(id INT PRIMARY KEY NOT nullptr";
         auto tuples = object.getTuplesFromStream(object.serialize());
@@ -65,17 +65,17 @@ public:
         int rc;
         char *sql;
 
-        rc = sqlite3_exec(db, querry.c_str(), nullptr, nullptr, &zErrMsg);
+        rc = sqlite3_exec(db_, querry.c_str(), nullptr, nullptr, &zErrMsg);
 
         if( rc != SQLITE_OK ) {
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
-            sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-            sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
             return false;
         }
-        sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-        sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
         return true;
     };
 
@@ -83,7 +83,7 @@ public:
 
     template <class ObjectType>
     bool save(ObjectType object) {
-        sqlite3_exec(this->db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
         std::string querry = "INSERT INTO "+ object.getTableName() + " VALUES (";
         int id = object.getId();
         if ( id == -1 ) {
@@ -104,16 +104,16 @@ public:
         char *zErrMsg = nullptr;
         int rc;
         char *sql;
-        rc = sqlite3_exec(db, querry.c_str(), selectCallback, &this->stream, &zErrMsg);
+        rc = sqlite3_exec(db_, querry.c_str(), selectCallback, &this->stream_, &zErrMsg);
         if( rc != SQLITE_OK ) {
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
-            sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-            sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
             return false;
         }
-        sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-        sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
         return true;
     };
 
@@ -121,53 +121,53 @@ public:
 
     template <class ObjectType>
     std::vector<ObjectType> all() {
-        sqlite3_exec(this->db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
-        this->stream.clear();
+        sqlite3_exec(this->db_, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+        this->stream_.clear();
         ObjectType object;
         std::vector<ObjectType> objects;
         std::string querry = "select * from "+ object.getTableName() + ";";
         char *zErrMsg = nullptr;
         int rc;
         char *sql;
-        rc = sqlite3_exec(db, querry.c_str(), selectCallback, &this->stream, &zErrMsg);
+        rc = sqlite3_exec(db_, querry.c_str(), selectCallback, &this->stream_, &zErrMsg);
         if( rc != SQLITE_OK ) {
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
-            sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
-            sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
             return objects;
         }
-        auto streams = object.getStreamsFromSerializedInput(this->stream);
+        auto streams = object.getStreamsFromSerializedInput(this->stream_);
         for (auto& auxStream : streams) {
             object.deserialize(auxStream);
             objects.emplace_back(object);
         }
-        sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-        sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
         return objects;
     };
 
     template <class ObjectType>
     ObjectType getById(int id) {
-        sqlite3_exec(this->db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
         ObjectType object;
-        this->stream.clear();
+        this->stream_.clear();
         std::string querry = "select * from "+ object.getTableName();
         querry += " where id is "+ std::to_string(id) +";";
         char *zErrMsg = nullptr;
         int rc;
         char *sql;
-        rc = sqlite3_exec(db, querry.c_str(), selectCallback, &this->stream, &zErrMsg);
+        rc = sqlite3_exec(db_, querry.c_str(), selectCallback, &this->stream_, &zErrMsg);
         if( rc != SQLITE_OK ) {
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
-            sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-            sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
             return object;
         }
-        object.deserialize(this->stream);
-        sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-        sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+        object.deserialize(this->stream_);
+        sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
         return object;
     };
 
@@ -175,8 +175,8 @@ public:
 
     template <class ObjectType>
     std::vector<ObjectType> getByField(const std::string& field, const std::string& value) {
-        sqlite3_exec(this->db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
-        this->stream.clear();
+        sqlite3_exec(this->db_, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+        this->stream_.clear();
         ObjectType object;
         std::vector<ObjectType> objects;
         std::string querry = "select * from "+ object.getTableName();
@@ -185,28 +185,28 @@ public:
         char *zErrMsg = nullptr;
         int rc;
         char *sql;
-        rc = sqlite3_exec(db, querry.c_str(), selectCallback, &this->stream, &zErrMsg);
+        rc = sqlite3_exec(db_, querry.c_str(), selectCallback, &this->stream_, &zErrMsg);
         if( rc != SQLITE_OK ) {
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
-            sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-            sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
             return objects;
         }
-        auto streams = object.getStreamsFromSerializedInput(this->stream);
+        auto streams = object.getStreamsFromSerializedInput(this->stream_);
         for (auto& auxStream : streams) {
             object.deserialize(auxStream);
             objects.emplace_back(object);
         }
-        sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-        sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
         return objects;
     };
 
     template <class ObjectType>
     bool update(ObjectType object) {
-        sqlite3_exec(this->db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
-        this->stream.clear();
+        sqlite3_exec(this->db_, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+        this->stream_.clear();
         std::string querry = "update "+ object.getTableName() + " set ";
         bool firstField = true;
         auto tuples = object.getTuplesFromStream(object.serialize());
@@ -220,24 +220,24 @@ public:
         char *zErrMsg = nullptr;
         int rc;
         char *sql;
-        rc = sqlite3_exec(db, querry.c_str(), selectCallback, &this->stream, &zErrMsg);
+        rc = sqlite3_exec(db_, querry.c_str(), selectCallback, &this->stream_, &zErrMsg);
         if( rc != SQLITE_OK ) {
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
-            sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-            sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
             return false;
         }
-        sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-        sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
         return true;
     };
 
 
     template <class ObjectType>
     bool remove(ObjectType object) {
-        sqlite3_exec(this->db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
-        this->stream.clear();
+        sqlite3_exec(this->db_, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+        this->stream_.clear();
         std::string querry = "delete from "+ object.getTableName() + " where id is " + std::to_string(object.getId()) + ";";
         bool firstField = true;
         auto tuples = object.getTuplesFromStream(object.serialize());
@@ -245,16 +245,16 @@ public:
         char *zErrMsg = nullptr;
         int rc;
         char *sql;
-        rc = sqlite3_exec(db, querry.c_str(), selectCallback, &this->stream, &zErrMsg);
+        rc = sqlite3_exec(db_, querry.c_str(), selectCallback, &this->stream_, &zErrMsg);
         if( rc != SQLITE_OK ) {
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
-            sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-            sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+            sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
             return false;
         }
-        sqlite3_exec(this->db, "END TRANSACTION;", nullptr, nullptr, nullptr);
-        sqlite3_exec(this->db, "COMMIT;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "END TRANSACTION;", nullptr, nullptr, nullptr);
+        sqlite3_exec(this->db_, "COMMIT;", nullptr, nullptr, nullptr);
         return true;
     };
 
